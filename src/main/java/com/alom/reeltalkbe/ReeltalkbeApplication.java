f077583de3797edf1b6cb4758e80c82e0b99f5b1
discipline24 <ejsky33@naver.com>
2025-01-24 22:10:45 +0900

    Initial commit

---
+++ src/main/java/com/alom/reeltalkbe/ReeltalkbeApplication.java
@@ -0,0 +1,13 @@
+package com.alom.reeltalkbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReeltalkbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReeltalkbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alom/reeltalkbe/ReeltalkbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alom.reeltalkbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReeltalkbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
