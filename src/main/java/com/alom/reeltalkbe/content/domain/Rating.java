3117c13b7083d4b287ce31d487bd4571081bf636
discipline24 <ejsky33@naver.com>
2025-02-04 19:07:16 +0900

    feat: 컨텐츠 조회, 평점 평가 기능 추가

--- src/main/java/com/alom/reeltalkbe/content/domain/Rating.java
+++ src/main/java/com/alom/reeltalkbe/content/domain/Rating.java
@@ -29,6 +29,7 @@ public class Rating extends BaseEntity {
 
     private Integer ratingValue; // 1~5점
 
+    public Rating() {}
 
     // todo : 빌더패턴 user 추가
     @Builder
@@ -36,4 +37,5 @@ public class Rating extends BaseEntity {
         this.content = content;
         this.ratingValue = ratingValue;
     }
+
 }
