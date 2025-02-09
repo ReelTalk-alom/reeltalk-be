7edbdb8b0341b24069892d326bbe011af4604ffa
soohyun1904 <dalsu4594@naver.com>
2025-02-09 16:16:48 +0900

    feat: ReviewRatingDto 추가(rating, ReviewId, userId)

---
+++ src/main/java/com/alom/reeltalkbe/review/dto/ReviewRatingDto.java
@@ -0,0 +1,15 @@
+package com.alom.reeltalkbe.review.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewRatingDto {
+    private Integer rating;
+    private Long ReviewId;
+    private Long userId;
+}
