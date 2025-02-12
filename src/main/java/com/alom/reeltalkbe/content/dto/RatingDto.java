84f3db50e2cdbfad0b9ffbafe48ff8aff48a7c5c
discipline24 <ejsky33@naver.com>
2025-02-12 20:49:41 +0900

    feat: 평점 관련 코드 추가

--- src/main/java/com/alom/reeltalkbe/content/dto/RatingDto.java
+++ src/main/java/com/alom/reeltalkbe/content/dto/RatingDto.java
@@ -1,9 +1,12 @@
 package com.alom.reeltalkbe.content.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
+// Dto 는 jackson 의 역직렬화 위해 Setter 나 생성자 필요
 public class RatingDto {
-    private Integer rating;
-    private Long user_id;
+    // todo : @NotNull , @Valid, bindingResult
+    private int rating;
 }
