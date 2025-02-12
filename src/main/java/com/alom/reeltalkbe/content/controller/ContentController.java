2751b7cdc1313a9ea957aab6f4735bb3e148689f
discipline24 <ejsky33@naver.com>
2025-02-12 20:49:20 +0900

    feat: 평점 추기 및 삭제 기능, 테스트용 기능 추가

--- src/main/java/com/alom/reeltalkbe/content/controller/ContentController.java
+++ src/main/java/com/alom/reeltalkbe/content/controller/ContentController.java
@@ -5,8 +5,9 @@ import com.alom.reeltalkbe.common.response.BaseResponse;
 import com.alom.reeltalkbe.content.domain.Content;
 import com.alom.reeltalkbe.content.dto.RatingDto;
 import com.alom.reeltalkbe.content.service.ContentService;
-import lombok.Getter;
+import com.alom.reeltalkbe.user.dto.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,31 +19,42 @@ public class ContentController {
     @GetMapping("/browse/tab")              // todo : type enum 만들기
     public BaseResponse<String> getContents(@RequestParam String type) {
         contentService.findPopularContentImages();
+        // todo : 포스터+백그라운드(가로) 이미지 두개 추가
         return new BaseResponse<>("흠, 어떡하쥥");
     }
 
     // 컨텐츠 세부 정보 조회
     @GetMapping("/contents/{contentId}")
-    public BaseResponse<Content> getContentInfo(@PathVariable Long content_id) {
+    public BaseResponse<Content> getContentInfo(@PathVariable Long contentId) {
         return new BaseResponse<>(contentService
-                .findContentById(content_id));
+                .findContentById(contentId));
     }
 
     // 컨텐츠 평점 생성
     @PostMapping("/contents/{contentId}")
-    public BaseResponse<Content> evaluateContentRating(@PathVariable Long content_id,
-                                                       RatingDto ratingDto) {
+    public BaseResponse<Content> evaluateContentRating(@PathVariable Long contentId,
+                                                       @RequestBody RatingDto ratingDto,
+                                                       @AuthenticationPrincipal CustomUserDetails userDetails) {
+        // RestController = (RequestBody XXX )ResponseBody + Controller
         return new BaseResponse<>(contentService
-                .addRating(content_id, ratingDto));
+                // todo : userDetail에 넣을 정보: username vs user Id -> userId이 보편적(성능우위, PK 성질)
+                .addRating(contentId, userDetails.getUsername(), ratingDto));
     }
 
     // 컨텐츠 평점 삭제
     @DeleteMapping("/contents/{contentId}")
-    public BaseResponse<Content> deleteContentRating(@PathVariable Long content_id,
-                                                     RatingDto ratingDto) {
+    public BaseResponse<Content> deleteContentRating(@PathVariable Long contentId,
+                                                     @AuthenticationPrincipal CustomUserDetails userDetails) {
         return new BaseResponse<>(contentService
-                .deleteRating(content_id, ratingDto.getUser_id()));
+                .deleteRating(contentId, userDetails.getUsername()));
     }
 
+
+    // --------------- 테스트용 메서드 ----------------------
+    // 컨텐츠 평점 생성
+    @GetMapping("/contents")
+    public BaseResponse<Content> addContent() {
+        return new BaseResponse<>(contentService.addContent(new Content()));
+    }
 
 }
--- src/main/java/com/alom/reeltalkbe/content/service/ContentService.java
+++ src/main/java/com/alom/reeltalkbe/content/service/ContentService.java
@@ -7,6 +7,7 @@ import com.alom.reeltalkbe.content.domain.Rating;
 import com.alom.reeltalkbe.content.dto.RatingDto;
 import com.alom.reeltalkbe.content.repository.ContentRepository;
 import com.alom.reeltalkbe.content.repository.RatingRepository;
+import com.alom.reeltalkbe.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ContentService {
 
+    private final UserRepository userRepository;
     private final ContentRepository contentRepository;
     private final RatingRepository ratingRepository;
 
@@ -27,34 +29,48 @@ public class ContentService {
 
     public Content findContentById(Long id) {
         return contentRepository.findById(id)
-                .orElseThrow(() -> new BaseException(BaseResponseStatus.INVALID_REQUEST));
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.CONTENT_NOT_FOUND));
     }
 
-    public Content addRating(Long content_id, RatingDto ratingDto) {
-        Content content = contentRepository.findById(content_id)
-                .orElseThrow(() -> new BaseException(BaseResponseStatus.CONTENT_NOT_FOUND));
+    public Content addRating(Long contentId, String username, RatingDto ratingDto) {
+        // 이미 평가한 컨텐츠라면 예외 처리
+        // todo: user pk 이름 id로 바꾸기? -> getUserId() 메서드도 getId() 로 바뀜
+        if (ratingRepository.findRatingByContentIdAndUserUsername(contentId, username).isPresent()) {
+            throw new BaseException(BaseResponseStatus.EXIST_RATING);
+        }
+        // content 조회
+        Content content = findContentById(contentId);
 
-        Rating rating = Rating.builder()
-                // todo : 빌더패턴 user 추가
+        // user 조회 후 rating 빌더 패턴 사용
+        Rating rating = Rating
+                .builder()
+                .user(userRepository.findByUsername(username)
+                        .orElseThrow(() -> new BaseException(BaseResponseStatus.NON_EXIST_USER)))
                 .content(content)
                 .ratingValue(ratingDto.getRating())
                 .build();
 
-        // 영속성 컨텍스트가 객체값 변경시 자동 변경사항 반영
+        // 컨텐츠 평균 평점 계산 후 rating 객체 DB에 저장
         content.updateRating(rating);
         ratingRepository.save(rating);
         return content;
     }
 
-    public Content deleteRating(Long content_id, Long user_id) {
-//        Content content = contentRepository.findById(content_id)
-//                .orElseThrow(() -> new BaseException(BaseResponseStatus.CONTENT_NOT_FOUND));
-//        Rating rating = ratingRepository.findRatingByContentAndUser(content_id, user_id)
-//                .orElseThrow(() -> new BaseException(BaseResponseStatus.EXIST_RATING));
-//
-//        content.deleteRating(rating);
-//        ratingRepository.delete(rating);
-//        return content;
-        return null;
+    public Content deleteRating(Long contentId, String username) {
+        // content, rating 조회
+        Content content = findContentById(contentId);
+        Rating rating = ratingRepository.findRatingByContentIdAndUserUsername(contentId, username)
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.RATING_NOT_FOUND));
+
+        content.deleteRating(rating);
+        ratingRepository.delete(rating);
+        return content;
+    }
+
+
+
+    // ------------------ 테스트용 메서드 ------------------------
+    public Content addContent(Content content) {
+        return contentRepository.save(content);
     }
 }
