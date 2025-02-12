6db01127f08bdb528c70d82d80e071e1443b84a9
discipline24 <ejsky33@naver.com>
2025-02-12 20:52:35 +0900

    feat: BaseException status 받아서 처리하는 Global 컨트롤러 추가

--- src/main/java/com/alom/reeltalkbe/global/GlobalExceptionHandler.java
+++ src/main/java/com/alom/reeltalkbe/global/GlobalExceptionHandler.java
@@ -1,8 +1,18 @@
 package com.alom.reeltalkbe.global;
 
+import com.alom.reeltalkbe.common.exception.BaseException;
+import com.alom.reeltalkbe.common.response.BaseResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    /**
+     * BaseException 예외 처리
+     */
+    @ExceptionHandler(BaseException.class)
+    public BaseResponse<String> handleBaseException(BaseException ex) {
+        return new BaseResponse<>(ex.getStatus());
+    }
 
 }
