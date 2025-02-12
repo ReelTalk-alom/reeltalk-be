09d2b06d5cf32a60da47920bc71339d76f9b5ded
discipline24 <ejsky33@naver.com>
2025-02-12 20:50:27 +0900

    feat: dto 추가

---
+++ src/main/java/com/alom/reeltalkbe/talk/dto/TalkMessageResponseDto.java
@@ -0,0 +1,56 @@
+package com.alom.reeltalkbe.talk.dto;
+
+import com.alom.reeltalkbe.talk.domain.TalkMessage;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class TalkMessageResponseDto {
+    private String message;
+    private String sender;
+    private Long contentId;
+    private Long messageId;
+    private Long userId;
+
+    @Builder
+    private TalkMessageResponseDto(String message, String sender, Long contentId, Long messageId, Long userId) {
+        this.message = message;
+        this.sender = sender;
+        this.contentId = contentId;
+        this.messageId = messageId;
+        this.userId = userId;
+    }
+
+    public static TalkMessageResponseDto of(TalkMessage message) {
+        return TalkMessageResponseDto
+                .builder()
+                .message(message.getMessage())
+                .sender(message.getSender())
+                .contentId(message.getContent().getId())
+                .userId(message.getUser().getUserId())
+                .messageId(message.getId())
+                .build();
+    }
+
+    public static List<TalkMessageResponseDto> dtoListOf(List<TalkMessage> messageList) {
+        List<TalkMessageResponseDto> list = new ArrayList<>();
+        for(TalkMessage message : messageList) {
+            list.add(
+                    TalkMessageResponseDto
+                            .builder()
+                            .message(message.getMessage())
+                            .sender(message.getSender())
+                            .contentId(message.getContent().getId())
+                            .userId(message.getUser().getUserId())
+                            .messageId(message.getId())
+                            .build()
+            );
+        }
+        return list;
+    }
+}
