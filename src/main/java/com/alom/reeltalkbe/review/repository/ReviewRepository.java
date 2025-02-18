0c17b3d81aa7d1978305c1f2a885cc88dc5e1f98
soohyun1904 <dalsu4594@naver.com>
2025-02-18 18:35:02 +0900

    feat: userId로 review검색

--- src/main/java/com/alom/reeltalkbe/review/repository/ReviewRepository.java
+++ src/main/java/com/alom/reeltalkbe/review/repository/ReviewRepository.java
@@ -7,10 +7,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReviewRepository extends JpaRepository <Review, Long> {
     Page<Review> findByContentId(Long contentId, Pageable pageable);
 
+    List<Review> findByUserId(Long userId);
+
+    Optional<Object> findTopByUserIdOrderByRatingSumDesc(Long userId);
 }
 
