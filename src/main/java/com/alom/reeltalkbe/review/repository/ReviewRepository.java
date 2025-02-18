1f8eb9102ef9bdb1dda1708c934d87d4bfc159ac
soohyun1904 <dalsu4594@naver.com>
2025-02-18 17:30:34 +0900

    feat: Review에 페이지 기능 추가, rating 기능 추가

--- src/main/java/com/alom/reeltalkbe/review/repository/ReviewRepository.java
+++ src/main/java/com/alom/reeltalkbe/review/repository/ReviewRepository.java
@@ -1,6 +1,8 @@
 package com.alom.reeltalkbe.review.repository;
 
 import com.alom.reeltalkbe.review.domain.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,7 @@ import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository <Review, Long> {
-    List<Review> findByContentId(Long contentId);
+    Page<Review> findByContentId(Long contentId, Pageable pageable);
+
 }
 
