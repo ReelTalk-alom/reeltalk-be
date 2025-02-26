a82eb414ebe699bf9912c7e7614aeed8497c98d8
discipline24 <ejsky33@naver.com>
2025-02-26 12:53:42 +0900

    feat : 컨텐츠 속성 이름 변경, 영화 탭, 컨텐츠 상세 조회 기능 추가

---
+++ src/main/java/com/alom/reeltalkbe/content/domain/ContentType.java
@@ -0,0 +1,5 @@
+package com.alom.reeltalkbe.content.domain;
+
+public enum ContentType{
+    MOVIE, SERIES
+}
---
+++ src/main/java/com/alom/reeltalkbe/content/domain/GenreListConverter.java
@@ -0,0 +1,39 @@
+package com.alom.reeltalkbe.content.domain;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import java.util.Collections;
+import java.util.List;
+
+@Converter
+public class GenreListConverter implements AttributeConverter<List<Genre>, String> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(List<Genre> genreList) {
+        if (genreList == null) {
+            return null;
+        }
+        try {
+            return objectMapper.writeValueAsString(genreList);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Error converting list of genres to JSON", e);
+        }
+    }
+
+    @Override
+    public List<Genre> convertToEntityAttribute(String json) {
+        if (json == null || json.isEmpty()) {
+            return Collections.emptyList();
+        }
+        try {
+            return objectMapper.readValue(json, new TypeReference<List<Genre>>() {});
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Error converting JSON to list of genres", e);
+        }
+    }
+}
