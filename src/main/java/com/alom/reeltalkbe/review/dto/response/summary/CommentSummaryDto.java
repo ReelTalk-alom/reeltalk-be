a9a8f8c7b0676625b16e83a3a9240ddfccb0673a
soohyun1904 <dalsu4594@naver.com>
2025-02-28 12:54:57 +0900

    feat: content 추가

--- src/main/java/com/alom/reeltalkbe/review/dto/response/summary/CommentSummaryDto.java
+++ src/main/java/com/alom/reeltalkbe/review/dto/response/summary/CommentSummaryDto.java
@@ -10,14 +10,15 @@ public class CommentSummaryDto {
     private final Long userId;
     private final String userName;
     private final String createdAt;
-
+    private final String content;
     private final int likeCount;
 
     @Builder
-    private CommentSummaryDto(Long userId, String userName, String createdAt, int likeCount) {
+    private CommentSummaryDto(Long userId, String userName, String createdAt, String conntent, int likeCount) {
         this.userId = userId;
         this.userName = userName;
         this.createdAt = createdAt;
+        this.content = conntent;
         this.likeCount = likeCount;
     }
 
@@ -26,6 +27,7 @@ public class CommentSummaryDto {
                 .userId(comment.getUser().getId())
                 .userName(comment.getUser().getUsername())
                 .createdAt(comment.getCreatedAt().toString())
+                .conntent(comment.getContent())
                 .likeCount(comment.getLikeCount())
                 .build();
     }
