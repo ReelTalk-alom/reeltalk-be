01d06f3cec03ccca4098b007ab670e2cfd52d22d
discipline24 <ejsky33@naver.com>
2025-03-02 16:05:02 +0900

    refactor : reviewResponse, talkMessageResponse 관련 리팩터링

--- src/main/java/com/alom/reeltalkbe/content/dto/ContentDetailsResponse.java
+++ src/main/java/com/alom/reeltalkbe/content/dto/ContentDetailsResponse.java
@@ -4,6 +4,7 @@ import com.alom.reeltalkbe.content.domain.Character;
 import com.alom.reeltalkbe.content.domain.Content;
 import com.alom.reeltalkbe.review.domain.Review;
 import com.alom.reeltalkbe.talk.domain.TalkMessage;
+import com.alom.reeltalkbe.talk.dto.TalkMessageResponse;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,23 +19,23 @@ public class ContentDetailsResponse {
     private final Content content;
 
     //private final List<Character> characters;
-    private final List<Review> reviews;
-    private final List<TalkMessage> talks;
+    private final List<ReviewResponse> reviews;
+    private final List<TalkMessageResponse> talks;
 
     @Builder
-    private ContentDetailsResponse(Content content, List<Review> reviews, List<TalkMessage> talks) {
+    private ContentDetailsResponse(Content content, List<ReviewResponse> reviews, List<TalkMessageResponse> talks) {
         this.content = content;
 //      this.characters = characters;
         this.reviews = reviews;
         this.talks = talks;
     }
 
-    public static ContentDetailsResponse of(Content content, List<Review> reviews, List<TalkMessage> talks) {
+    public static ContentDetailsResponse of(Content content, List<ReviewResponse> reviews, List<TalkMessage> talks) {
         return ContentDetailsResponse.builder()
                 .content(content)
 //              .characters(characters)
                 .reviews(reviews)
-                .talks(talks)
+                .talks(TalkMessageResponse.dtoListOf(talks))
                 .build();
     }
 }
