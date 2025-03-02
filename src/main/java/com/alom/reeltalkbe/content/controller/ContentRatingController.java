75d5a19ba8cf74f1761d48f415fda76969174324
discipline24 <ejsky33@naver.com>
2025-03-02 16:02:39 +0900

    refactor : uri @RequestMapping 및 ratingDto 이름변경

--- src/main/java/com/alom/reeltalkbe/content/controller/ContentRatingController.java
+++ src/main/java/com/alom/reeltalkbe/content/controller/ContentRatingController.java
@@ -3,13 +3,14 @@ package com.alom.reeltalkbe.content.controller;
 
 import com.alom.reeltalkbe.common.response.BaseResponse;
 import com.alom.reeltalkbe.content.domain.Content;
-import com.alom.reeltalkbe.content.dto.RatingDto;
+import com.alom.reeltalkbe.content.dto.RatingRequest;
 import com.alom.reeltalkbe.content.service.ContentRatingService;
 import com.alom.reeltalkbe.user.dto.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/api/contents/{contentId}")
 @RestController
 @RequiredArgsConstructor
 public class ContentRatingController {
@@ -18,17 +19,17 @@ public class ContentRatingController {
 
 
     // 컨텐츠 평점 생성
-    @PostMapping("/contents/{contentId}")
+    @PostMapping
     public BaseResponse<Content> evaluateContentRating(@PathVariable Long contentId,
-                                                       @RequestBody RatingDto ratingDto,
+                                                       @RequestBody RatingRequest ratingRequest,
                                                        @AuthenticationPrincipal CustomUserDetails userDetails) {
         // RestController = (RequestBody XXX )ResponseBody + Controller
         return new BaseResponse<>(contentRatingService
-                .addRating(contentId, userDetails.getUserId(), ratingDto));
+                .addRating(contentId, userDetails.getUserId(), ratingRequest));
     }
 
     // 컨텐츠 평점 삭제
-    @DeleteMapping("/contents/{contentId}")
+    @DeleteMapping
     public BaseResponse<Content> deleteContentRating(@PathVariable Long contentId,
                                                      @AuthenticationPrincipal CustomUserDetails userDetails) {
         return new BaseResponse<>(contentRatingService
