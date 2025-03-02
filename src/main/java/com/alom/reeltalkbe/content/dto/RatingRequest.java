76a74be35604637638184262f78eac44482a360a
discipline24 <ejsky33@naver.com>
2025-03-02 15:58:50 +0900

    refactor : dto 이름 변경

--- src/main/java/com/alom/reeltalkbe/content/dto/RatingRequest.java
+++ src/main/java/com/alom/reeltalkbe/content/dto/RatingRequest.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 // Dto 는 jackson 의 역직렬화 위해 Setter 나 생성자 필요
-public class RatingDto {
+public class RatingRequest {
     // todo : @NotNull , @Valid, bindingResult
     private int rating;
 }
