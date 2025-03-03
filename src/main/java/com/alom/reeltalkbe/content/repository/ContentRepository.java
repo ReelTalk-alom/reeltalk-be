73e4cb6908559d0d184172f159f998afa5aa9343
discipline24 <ejsky33@naver.com>
2025-03-03 20:48:23 +0900

    feat : 분류명 쿼리 파라미터에 따른 영화 분류 코드 추가

--- src/main/java/com/alom/reeltalkbe/content/repository/ContentRepository.java
+++ src/main/java/com/alom/reeltalkbe/content/repository/ContentRepository.java
@@ -2,10 +2,14 @@ package com.alom.reeltalkbe.content.repository;
 
 
 import com.alom.reeltalkbe.content.domain.Content;
+import java.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ContentRepository extends JpaRepository<Content, Long> {
-    List<Content> findTop10ByOrderByReleaseDateAsc();
+    List<Content> findTop10ByOrderByReleaseDateDesc();
+    List<Content> findTop10ByOrderByRatingAverageDesc();
+    List<Content> findTop10ByReleaseDateBetweenOrderByPopularityDesc(LocalDate today, LocalDate monthLater);
+    List<Content> findTop10ByReleaseDateAfterOrderByReleaseDateAsc(LocalDate today);
 }
--- src/main/java/com/alom/reeltalkbe/content/service/ContentService.java
+++ src/main/java/com/alom/reeltalkbe/content/service/ContentService.java
@@ -12,6 +12,7 @@ import com.alom.reeltalkbe.review.repository.ReviewRepository;
 import com.alom.reeltalkbe.talk.domain.TalkMessage;
 import com.alom.reeltalkbe.talk.repository.TalkMessageRepository;
 import jakarta.transaction.Transactional;
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -42,36 +43,48 @@ public class ContentService {
     public ContentDetailsResponse findContentDetailsByContentId(Long contentId) {
         Content content = contentRepository.findById(contentId)
                 .orElseThrow(() -> new BaseException(BaseResponseStatus.CONTENT_NOT_FOUND));
-        // todo : jpa orderby 좋아요수? 같은거 추가
-        List<ReviewResponse> reviews = reviewRepository.findAllByContentId(content.getId())
+
+        List<ReviewResponse> reviews = reviewRepository.findTop10ByContentIdOrderByReviewLikesDesc(content.getId())
             .stream()
             .map(ReviewResponse::of)
             .toList();
+
         // todo : talkMessages 시간순으로 나오는지 보기
         List<TalkMessage> talkMessages = talkMessageRepository.findAllByContentId(content.getId());
         return ContentDetailsResponse.of(content, reviews, talkMessages);
     }
 
     public List<MovieTabResponse> findMoviesAndReviewsSortBy(String sort) {
-        List<Content> contentList = new ArrayList<>();
 
-        if(sort.equals("releaseDate")) {    // todo : 분류명 추가 가능
-            contentList = contentRepository.findTop10ByOrderByReleaseDateAsc();
+        List<Content> contentList = new ArrayList<>();
+        switch (sort) {
+            case "release-date" ->          // 개봉일자 오름차순 반환
+                contentList = contentRepository.findTop10ByOrderByReleaseDateDesc();
+            case "top-rated" ->             // 별점 내림차순 반환
+                contentList = contentRepository.findTop10ByOrderByRatingAverageDesc();
+            case "now-playing" ->           // 오늘 이전에 개봉한 영화 리스트 인기순 반환
+                contentList = contentRepository.findTop10ByReleaseDateBetweenOrderByPopularityDesc(LocalDate.now().minusMonths(1), LocalDate.now().plusMonths(1));
+            case "up-coming" ->             // 오늘 이후에 개봉할 영화를 개봉일자 오름차순으로 반환
+                contentList = contentRepository.findTop10ByReleaseDateAfterOrderByReleaseDateAsc(LocalDate.now());
+            default ->                      // 분류기준 잘못되면 예외 처리
+                throw new BaseException(BaseResponseStatus.INVALID_QUERY_PARAMETER);
         }
 
         List<Long> contentIds = contentList.stream()
                 .map(Content::getId)
-                .collect(Collectors.toList());
+                .toList();
 
-        // todo : jpa orderby 좋아요수? 같은거 추가
-        List<Review> reviewList = reviewRepository.findTop10ByContentIdIn(contentIds);
+        List<Review> reviewList = reviewRepository
+                .findTop10ByContentIdInOrderByReviewLikesDesc(contentIds);
 
         Map<Long, List<Review>> reviewsByContent = reviewList.stream()
                 .collect(Collectors.groupingBy(review -> review.getContent().getId()));
 
         return contentList.stream()
-                .map(content -> MovieTabResponse.of(content, reviewsByContent
-                    .getOrDefault(content.getId(), Collections.emptyList())))
+                .map(content -> MovieTabResponse.of(
+                    content,
+                    reviewsByContent.getOrDefault(content.getId(), Collections.emptyList()))
+                )
                 .toList();
     }
 
