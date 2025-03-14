42faeef9ed80004e2c38b76af6dedf727c9c8a1b
discipline24 <ejsky33@naver.com>
2025-03-15 00:05:52 +0900

    refactor: 리포지토리 top10 삭제 및 중복 제거

--- src/main/java/com/alom/reeltalkbe/content/repository/ContentRepository.java
+++ src/main/java/com/alom/reeltalkbe/content/repository/ContentRepository.java
@@ -12,21 +12,15 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ContentRepository extends JpaRepository<Content, Long> {
-
-    List<Content> findTop10ByContentTypeOrderByReleaseDateDesc(ContentType contentType);
-    List<Content> findTop10ByContentTypeOrderByRatingAverageDesc(ContentType contentType);
-    List<Content> findTop10ByContentTypeAndReleaseDateBetweenOrderByPopularityDesc(ContentType contentType, LocalDate today, LocalDate monthLater);
-    List<Content> findTop10ByContentTypeAndReleaseDateAfterOrderByReleaseDateAsc(ContentType contentType, LocalDate today);
-
-    List<Content> findTop10ByContentTypeOrderByReleaseDateAsc(ContentType contentType);
-
+    List<Content> findByContentTypeOrderByReleaseDateDesc(ContentType contentType);
     List<Content> findByContentTypeOrderByRatingAverageDesc(ContentType contentType);
+    List<Content> findByContentTypeAndReleaseDateBetweenOrderByPopularityDesc(ContentType contentType, LocalDate today, LocalDate monthLater);
 
     // releaseDate가 오늘 이전인 컨텐츠(내림차순)
-    List<Content> findByContentTypeAndReleaseDateBeforeOrderByReleaseDateDesc(ContentType contentType, LocalDate date);
+    List<Content> findByContentTypeAndReleaseDateBeforeOrderByReleaseDateDesc(ContentType contentType, LocalDate today);
 
     // releaseDate가 오늘 이후인 컨텐츠(오름차순)
-    List<Content> findByContentTypeAndReleaseDateAfterOrderByReleaseDateAsc(ContentType contentType, LocalDate date);
+    List<Content> findByContentTypeAndReleaseDateAfterOrderByReleaseDateAsc(ContentType contentType, LocalDate today);
 
     // 기존에 존재하는 TV 시리즈의 제목과 개봉일 조회
     @Query("SELECT c.enTitle, c.releaseDate FROM Content c WHERE c.enTitle IN :titles AND c.releaseDate IN :dates")
